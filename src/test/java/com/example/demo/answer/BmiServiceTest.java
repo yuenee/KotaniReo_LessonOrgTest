3ced9916d549e7ac2a4614cd0256ea9f34342ebf
yukap <yukaperogrammer@gmail.com>
2024-09-29 22:04:33 +0900

    各値を変数に代入(マジックナンバーを無くす)

--- src/test/java/com/example/demo/answer/BmiServiceTest.java
+++ src/test/java/com/example/demo/answer/BmiServiceTest.java
@@ -12,6 +12,16 @@ class BmiServiceTest {
 	void BMIが正しく計算される() {
 		BmiService bmiService = new BmiService();
 
-		assertEquals(23.5, bmiService.getBmiCalcResult(1.75, 72), 0.1);
+		/* データの準備 */
+		double mHeight = 1.75;
+		double kgWeight = 72.0;
+		double delta = 0.1;        // 誤差の範囲(double型の検証のため、許容誤差が必要)
+		double expectedBmi = 23.5; // 期待値
+
+		/* テスト対象メソッド実行 */
+		double actualBmi = bmiService.getBmiCalcResult(mHeight, kgWeight);
+
+		/* 検証 */
+		assertEquals(expectedBmi, actualBmi, delta);
 	}
 }
